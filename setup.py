1fe4eb5b0b693d514a7ceb5d71d91fa0cac333d1
Mart Roosmaa <roosmaa@gmail.com>
2018-12-05 11:28:49 +0100

    Add missing ed25519 package dependency

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.0',
+    version='0.9.1',
 
     description='APIs for interacting with the RISE blockchain',
     long_description=long_description,
@@ -71,7 +71,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests'],
+    install_requires=[
+        'requests',
+        'ed25519',
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
